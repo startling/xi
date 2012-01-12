16e19d30a6983e1c0f271eb3a545f3f7e2622f99
tim <tdixon51793@gmail.com>
2012-01-12 05:41:05 -0600

    Doesn't need to be an argent thing.
    It was barely a command-line application; now it's even less of one and
    a little more lightweight.

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(
     author_email = "tdixon51793@gmail.com",
     description = "A little script for copying through unix pipes.",
     scripts = ['scripts/xi'],
-    install_requires = ['argent', 'clint', 'xerox'],
+    install_requires = ['clint', 'xerox'],
 )
