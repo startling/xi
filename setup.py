a820d0743a61de1f23355b49be8e1204643eec9d
tim <tdixon51793@gmail.com>
2012-01-12 17:21:01 -0600

    Added stuff to the `setup.py` for Pypi.

--- setup.py
+++ setup.py
@@ -6,7 +6,14 @@ setup(
     version = "0.0dev",
     author = "startling",
     author_email = "tdixon51793@gmail.com",
-    description = "A little script for copying through unix pipes.",
+    description = "A little script for copying and pasting through unix pipes.",
     scripts = ['scripts/xi'],
     install_requires = ['clint', 'xerox'],
+    keywords = ['paste', 'copy', 'copypaste', 'pipes'],
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Environment :: Console"
+    ]
 )
